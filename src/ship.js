54c650ebd0578760e9d1854291b2b15180ca7fb9
wyhong3103 <wyhong3103@gmail.com>
2022-10-16 14:49:21 +0800

    Add shipHead and isHorizontal parameter for a ship

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,8 @@
-const Ship = ((shipLength) => {
+const Ship = ((shipLength, head, isHorizontal) => {
 
     let totalHits = 0;
+    let shipHead = head;
+    let isHor = isHorizontal;
 
     function hit(){
         totalHits += 1;
@@ -10,10 +12,30 @@ const Ship = ((shipLength) => {
         return totalHits === shipLength;
     }
 
+    function setHead(head){
+        shipHead = head;
+    }
+
+    function getHead(){
+        return shipHead
+    }
+
+    function setHor(isHorizontal){
+        isHor = isHorizontal;
+    }
+
+    function getHor(){
+        return isHor;
+    }
+
     return{
         hit,
         isSunk,
-        shipLength
+        shipLength,
+        setHead,
+        getHead,
+        setHor,
+        getHor
     }
 });
 
