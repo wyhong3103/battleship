3bd702f0e0fbb63ff2588581bc6651083c1725f5
wyhong3103 <wyhong3103@gmail.com>
2022-10-16 14:48:48 +0800

    Update ship test with extra para

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,25 +1,25 @@
 import Ship from "../ship";
 
 test("Able to retrieve the length", () => {
-    const ship = Ship(4);
+    const ship = Ship(4, [0,0], true);
     expect(ship.shipLength).toBe(4);
 });
 
 test("A ship with length 1 should sunk with one hit", () => {
-    const ship = Ship(1);
+    const ship = Ship(1, [0,0], true);
     ship.hit();
     expect(ship.isSunk()).toBe(true);
 });
 
 test("A ship with length 2 shouldn't sunk with one hit", () => {
-    const ship = Ship(2);
+    const ship = Ship(2, [0,0], true);
     ship.hit();
     expect(ship.isSunk()).toBe(false);
 });
 
 test("Random test", () => {
     const length = Math.floor(Math.random() * 100);
-    const ship = Ship(length);
+    const ship = Ship(length, [0,0], true);
     let hitCount = 0;
     for(let i = 0; i < Math.floor(Math.random() * length); i++){
         ship.hit();
