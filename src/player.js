8783482e37a79fb22104e8b3abdbe5c6742fc61c
wyhong3103 <wyhong3103@gmail.com>
2022-10-17 12:21:37 +0800

    Add random ship placement func

--- src/player.js
+++ src/player.js
@@ -29,11 +29,26 @@ const Player = (() => {
         return res;
     }
 
+    function generateRandomShip(){
+        const ships = [2, 3, 3, 4, 5];
+        while (ships.length > 0){
+            const currentLength = ships[ships.length-1];
+            const [row,col] = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]            
+            const isHorizontal = Math.floor(Math.random() * 2) === 1;
+            
+            if (gameboard.isValidPlacement(currentLength,row, col, isHorizontal)){
+                gameboard.placeShip(currentLength, row, col, isHorizontal);
+                ships.pop();
+            }
+        }
+    }
+
     generateAttackOrder();
 
     return {
         gameboard,
         randomAttack,
+        generateRandomShip
     }
 });
 
